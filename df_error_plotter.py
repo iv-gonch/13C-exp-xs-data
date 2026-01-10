f30ce732168007652d9cc1780428d917ce1089bc
Ваня <iv.gonch.0907@gmail.com>
2026-01-11 00:55:11 +0300

    error plots bugfix

--- df_error_plotter.py
+++ df_error_plotter.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 
 def plot_xs_from_csv(name):
     # Чтение данных
-    df = pd.read_csv('./full_data_corrected/' + name +'_corrected.csv')
+    df = pd.read_csv('./full_data_corrected/' + name + '_corrected.csv')
 
     # Обязательные столбцы
     if 'XS (b)' not in df.columns or 'Ea (eV)' not in df.columns:
         raise ValueError("В файле должны быть столбцы 'XS (b)' и 'Ea (eV)'")
 
-    x = df['Ea (eV)']/1e6
+    x = df['Ea (eV)']
     y = df['XS (b)']
 
     # Необязательные погрешности
-    xerr = df['dEa (eV)']/1e6 if 'dEa (eV)' in df.columns else None
-    yerr = df['dXS (b)'] if 'dXS (b)' in df.columns else None
+    xerr = df['dEa (eV)'] if 'dEa (eV)' in df.columns else None
+    yerr = df['dXS (b)']  if 'dXS (b)'  in df.columns else None
 
     # Построение графика
     plt.figure(figsize=(7, 5), dpi=200)
@@ -23,15 +23,21 @@ def plot_xs_from_csv(name):
         x, y,
         xerr=xerr,
         yerr=yerr,
-        fmt='o',
+        fmt='.',
         capsize=3,
         elinewidth=1,
         markersize=4
     )
 
-    plt.xlabel('Ea (MeV)')
+    title = name + ' XS(Ea)' 
+    if not 'dXS (b)' in df.columns:
+        title += ' no XS errors'
+    if not 'dEa (eV)' in df.columns: 
+        title += ' no Ea errors'
+    
+    plt.xlabel('Ea (eV)')
     plt.ylabel('XS (b)')
-    plt.title(name + ' XS(Ea)')
+    plt.title(title)
     plt.grid(True)
 
     plt.tight_layout()
@@ -39,16 +45,16 @@ def plot_xs_from_csv(name):
 
 
 fnames = [
-    '0_Drotleff_1993',
-    '1_Bair_1973',
-    '2_Kellogg_1989',
-    '3_Febbraro_2020',
-    '4_Walton_1957',
-    '5_Brandenburg_2023',
-    '6_Sekharan_1967',
-    '7_Davids_1968',
-    '9_Prusachenko_2022',
-    '10_Gao_2022',
+    '1_Drotleff_1993',
+    '2_Bair_1973',
+    '3_Kellogg_1989',
+    '4_Febbraro_2020',
+    '5_Walton_1957',
+    '6_Brandenburg_2023',
+    '7_Sekharan_1967',
+    '8_Davids_1968',
+    '10_Prusachenko_2022',
+    '11_Gao_2022',
     '100_Mohr']
 
 for name in fnames:
